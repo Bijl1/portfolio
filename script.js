6aabdb62d00decd305269ad1a6715c763ba01bd3
Jonathan Bijl <sannoayo@gmail.com>
2023-10-05 19:00:21 -0400

    typing script

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    (function ($) {
+      $.fn.writeText = function (content) {
+        var contentArray = content.split(""),
+          current = 0,
+          elem = this;
+        setInterval(function () {
+          if (current < contentArray.length) {
+            elem.text(elem.text() + contentArray[current++]);
+          }
+        }, 90);
+      };
+    })(jQuery);
+
+    $("#holder").writeText("</> FRONT-END + BACK-END DEVELOPER </>");
+  });
