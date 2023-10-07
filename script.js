6278240bd917e629c6baf51bf6f222cba2aa35f9
Jonathan Bijl <sannoayo@gmail.com>
2023-10-07 09:27:02 -0400

    some updates

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 $(document).ready(function () {
-    (function ($) {
-      $.fn.writeText = function (content) {
-        var contentArray = content.split(""),
-          current = 0,
-          elem = this;
-        setInterval(function () {
-          if (current < contentArray.length) {
-            elem.text(elem.text() + contentArray[current++]);
-          }
-        }, 90);
-      };
-    })(jQuery);
+  (function ($) {
+    $.fn.writeText = function (content) {
+      var contentArray = content.split(""),
+        current = 0,
+        elem = this;
+      setInterval(function () {
+        if (current < contentArray.length) {
+          elem.text(elem.text() + contentArray[current++]);
+        }
+      }, 90);
+    };
+  })(jQuery);
 
-    $("#holder").writeText("</> FRONT-END + BACK-END DEVELOPER </>");
-  });
+  $("#holder").writeText("</> FRONT-END + BACK-END DEVELOPER </>");
+
+});
